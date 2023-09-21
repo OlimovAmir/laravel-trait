c39c09543610da70857f2c03344e25f429ad7f7c
Olimov Amir <olimovamir086@gmail.com>
2023-09-21 11:07:06 +0300

    start

---
+++ resources/views/layout/base.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Trait</title>
+</head>
+<body>
+    <header> @yield('contentHeader1')
+        <h1>Trait</h1>
+    </header>
+    <aside></aside>
+    <main></main>
+    <footer></footer>
+</body>
+</html>
